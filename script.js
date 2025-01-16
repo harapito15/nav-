1c7e66426a5074abbbc1bd17faaf692fc9c0033d
harapito15 <100458839+harapito15@users.noreply.github.com>
2025-01-16 22:42:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const Hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector ('.navbar-nav');
+const link = document.querySelector ('.link')
+ Hamburger.addEventListener('click', function(){
+  nav.classList.toggle('slide')
+  });
+  
